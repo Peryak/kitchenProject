ebfa6c57ff39cd1fce0e9a5e35e7be46a40ee83a
Pierre <riviera_paradise@hotmail.fr>
2016-06-15 14:03:36 +0200

    archi MVC

---
+++ Model/basicModel.php
@@ -0,0 +1,17 @@
+<?php
+
+// Every functions related to databases
+
+function dbConnect($bddname)
+{
+    $dsn = "mysql:host=localhost;dbname=$bddname;charset=utf8";
+    $user = 'root';
+    $password = 'root';
+
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        echo 'Connexion failed : ' . $e->getMessage();
+    }
+    return ($dbh);
+}
---
+++ View/Template/404.tpl
@@ -0,0 +1,5 @@
+<div class="positionError">
+    <img src="Asset/img/turner.jpg" class="imgError">
+    <h1 class="titleError">Error 404</h1>
+    <h2 class="paragraphError">Wrong Way</h2>
+</div>
