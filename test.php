3bd157ca5e3cec88eccbb3d25df6baa6b2aa306c
Pierre <riviera_paradise@hotmail.fr>
2016-07-06 08:43:48 +0200

    recup travail hugo

--- test.php
+++ test.php
@@ -8,7 +8,6 @@ $pdo = Database::connect();
 $var = new Read();
 $var->tab = $var->receiptAction($pdo, 'recette de test');
 
-<<<<<<< HEAD
 ?>
 
 <table>
@@ -26,7 +25,4 @@ $var->tab = $var->receiptAction($pdo, 'recette de test');
     </tbody>
 
 }
-    </table>
-=======
-var_dump($var->tab);
->>>>>>> 8d1093c83cc66d9619c25471367ad29e7e7b665b
+</table>
