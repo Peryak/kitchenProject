f5ccbb80941c7151739fb07d5e5d99b756255d1e
Pierre <riviera_paradise@hotmail.fr>
2016-07-16 14:32:16 +0200

    home panels and form

--- views/pages/home.php
+++ views/pages/home.php
@@ -19,7 +19,11 @@ $_SESSION['tab1'] = $var->getAllReceipt($pdo, TRUE, array(4));
 
 <div class="row">
     <!-- /.col-lg-6 -->
-<?php foreach ($_SESSION['tab1'] as $key) { ?>
+<?php
+    $t = 0;
+    foreach ($_SESSION['tab1'] as $key) {
+    $t += 1;
+        ?>
       <div class="col-lg-6">
         <div class="panel panel-default">
             <div class="panel-heading panelheading-clara_global">
@@ -30,24 +34,32 @@ $_SESSION['tab1'] = $var->getAllReceipt($pdo, TRUE, array(4));
 
                 <!-- Nav tabs -->
                 <ul class="nav nav-tabs">
-                    <li class="active"><a href="#step_1" data-toggle="tab">Receipt</a>
+                    <li class="active"><a href="#step_1<?php echo $t ?>" data-toggle="tab">Receipt</a>
                     </li>
-                    <li><a href="#step_2" data-toggle="tab">Ingredients</a>
+                    <li><a href="#step_2<?php echo $t ?>" data-toggle="tab">Ingredients</a>
                     </li>
                 </ul>
 
                 <!-- Tab panes -->
                 <div class="tab-content">
-                    <div class="tab-pane fade in active" id="step_1">
+                    <div class="tab-pane fade in active" id="step_1<?php echo $t ?>">
                         <div class="col-md-12">
                             <h5>Make a <em class="titlebodypanel-clara_global"><?php echo $key->getName(); ?></em> !</h5>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         </div>
                     </div>
-                    <div class="tab-pane fade" id="step_2">
+                    <div class="tab-pane fade" id="step_2<?php echo $t ?>">
                         <h5>Ingredients</h5>
-                        <?php echo "<tr>" . $key->getIngredients() . "</tr>"; ?>
+                        <?php
+                        /*foreach ($key->getIngredient() as $elements) {
+                            var_dump($elements);
+                            foreach ($elements as $ingredients) {
+                                echo $ingredients;
+                            }
+                        }*/
+                        //echo "<tr>" . $key->getIngredients() . "</tr>";
+                        ?>
                     </div>
                 </div>
 
@@ -66,6 +78,200 @@ $_SESSION['tab1'] = $var->getAllReceipt($pdo, TRUE, array(4));
     <!-- /.col-lg-6 -->
 <?php } ?>
 </div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading panelheading-clara_global">
+                <span class="titlepanel-clara_global">Basic Form Elements</span>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <form role="form">
+                            <div class="form-group">
+                                <label>Text Input</label>
+                                <input class="form-control">
+                                <p class="help-block">Example block-level help text here.</p>
+                            </div>
+                            <div class="form-group">
+                                <label>Text Input with Placeholder</label>
+                                <input class="form-control" placeholder="Enter text">
+                            </div>
+                            <div class="form-group">
+                                <label>Static Control</label>
+                                <p class="form-control-static">email@example.com</p>
+                            </div>
+                            <div class="form-group">
+                                <label>File input</label>
+                                <input type="file">
+                            </div>
+                            <div class="form-group">
+                                <label>Text area</label>
+                                <textarea class="form-control" rows="3"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Checkboxes</label>
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" value="">Checkbox 1
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" value="">Checkbox 2
+                                    </label>
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" value="">Checkbox 3
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Inline Checkboxes</label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox">1
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox">2
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="checkbox">3
+                                </label>
+                            </div>
+                            <div class="form-group">
+                                <label>Radio Buttons</label>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Radio 1
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Radio 2
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Radio 3
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Inline Radio Buttons</label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="optionsRadiosInline" id="optionsRadiosInline1" value="option1" checked>1
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="optionsRadiosInline" id="optionsRadiosInline2" value="option2">2
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="optionsRadiosInline" id="optionsRadiosInline3" value="option3">3
+                                </label>
+                            </div>
+                            <div class="form-group">
+                                <label>Selects</label>
+                                <select class="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Multiple Selects</label>
+                                <select multiple class="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit Button</button>
+                            <button type="reset" class="btn btn-default">Reset Button</button>
+                        </form>
+                    </div>
+                    <!-- /.col-lg-6 (nested) -->
+                    <div class="col-lg-6">
+                        <h1>Disabled Form States</h1>
+                        <form role="form">
+                            <fieldset disabled>
+                                <div class="form-group">
+                                    <label for="disabledSelect">Disabled input</label>
+                                    <input class="form-control" id="disabledInput" type="text" placeholder="Disabled input" disabled>
+                                </div>
+                                <div class="form-group">
+                                    <label for="disabledSelect">Disabled select menu</label>
+                                    <select id="disabledSelect" class="form-control">
+                                        <option>Disabled select</option>
+                                    </select>
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox">Disabled Checkbox
+                                    </label>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Disabled Button</button>
+                            </fieldset>
+                        </form>
+                        <h1>Form Validation States</h1>
+                        <form role="form">
+                            <div class="form-group has-success">
+                                <label class="control-label" for="inputSuccess">Input with success</label>
+                                <input type="text" class="form-control" id="inputSuccess">
+                            </div>
+                            <div class="form-group has-warning">
+                                <label class="control-label" for="inputWarning">Input with warning</label>
+                                <input type="text" class="form-control" id="inputWarning">
+                            </div>
+                            <div class="form-group has-error">
+                                <label class="control-label" for="inputError">Input with error</label>
+                                <input type="text" class="form-control" id="inputError">
+                            </div>
+                        </form>
+                        <h1>Input Groups</h1>
+                        <form role="form">
+                            <div class="form-group input-group">
+                                <span class="input-group-addon">@</span>
+                                <input type="text" class="form-control" placeholder="Username">
+                            </div>
+                            <div class="form-group input-group">
+                                <input type="text" class="form-control">
+                                <span class="input-group-addon">.00</span>
+                            </div>
+                            <div class="form-group input-group">
+                                            <span class="input-group-addon"><i class="fa fa-eur"></i>
+                                            </span>
+                                <input type="text" class="form-control" placeholder="Font Awesome Icon">
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon">$</span>
+                                <input type="text" class="form-control">
+                                <span class="input-group-addon">.00</span>
+                            </div>
+                            <div class="form-group input-group">
+                                <input type="text" class="form-control">
+                                            <span class="input-group-btn">
+                                                <button class="btn btn-default" type="button"><i class="fa fa-search"></i>
+                                                </button>
+                                            </span>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.col-lg-6 (nested) -->
+                </div>
+                <!-- /.row (nested) -->
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
 
 <!-- <p>Hello there <?php echo $first_name . ' ' . $last_name; ?>!<p>
 
